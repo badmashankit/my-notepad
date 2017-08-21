30b0a45f6c8e36495697a183836fa428684b6d7d
badmashankit <ankits.2711@gmail.com>
2017-08-22 05:24:17 +0530

    Added menu bar with menu items, fixed textarea

--- src/com/badmashankit/notepad/MyNotepad.java
+++ src/com/badmashankit/notepad/MyNotepad.java
@@ -17,28 +17,58 @@
 
 package com.badmashankit.notepad;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class MyNotepad extends JFrame {
 	private static final long serialVersionUID = 1L;
+	private JTextArea textArea;
+	private JMenuBar menuBar;
+	private JMenu file;
+	private JMenu edit;
+	private JMenu format;
+	private JMenu help;
+	private JMenuItem newFile;
+	private JMenuItem openFile;
+	private JMenuItem saveFile;
+	private JMenuItem exitFile;
 
 	public MyNotepad() {
 		super("MyNotepad");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setMinimumSize(new Dimension(500, 500));
+		textArea = new JTextArea();
+		menuBar = new JMenuBar();
+		file = new JMenu("File");
+		edit = new JMenu("Edit");
+		format = new JMenu("Format");
+		help = new JMenu("Help");
+		newFile = new  JMenuItem("New             Ctrl+N");
+		openFile = new JMenuItem("Open            Ctrl+O");
+		saveFile = new JMenuItem("Save            Ctrl+S");
+		exitFile = new JMenuItem("Exit");
 		setupView();
 		setVisible(true);
 	}
-	
+
 	public void setupView() {
-		JPanel mainPanel = new JPanel();
-		mainPanel.add(new JScrollPane(new JTextArea(20, 100)));
-		getContentPane().add(mainPanel);
-		pack();
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setMinimumSize(new Dimension(500, 500));
+		setLayout(new BorderLayout());
+		setJMenuBar(menuBar);
+		add(new JScrollPane(textArea));
+		menuBar.add(file);
+		menuBar.add(edit);
+		menuBar.add(format);
+		menuBar.add(help);
+		file.add(newFile);
+		file.add(openFile);
+		file.add(saveFile);
+		file.add(exitFile);
 	}
 }
